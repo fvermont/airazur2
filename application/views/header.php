c6003b293f8fa0ba94a00f98dfd15084fd1ed856
ginius4ginius <derieu.vincent@outlook.com>
2018-01-22 16:28:53 +0100

    commit

--- application/views/header.php
+++ application/views/header.php
@@ -3,7 +3,7 @@
   <center>  <img src="../img/banniere.png" alt="logo"></center>
   </div>
   <div class="menu">
-    <?php this->load->view("menu.php")
+    <?php this->load->view("menu.php");
     //include_once('menu.php')?>
   </div>
 </header>
